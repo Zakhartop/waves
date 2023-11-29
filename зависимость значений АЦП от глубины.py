be804dc748b627b1c924973343b88d4c7eeb7b96
Ilya Victorovich <145442503+FrozenFront@users.noreply.github.com>
2023-11-29 14:39:47 +0300

    Update зависимость значений АЦП от глубины.py

--- зависимость значений АЦП от глубины.py
+++ зависимость значений АЦП от глубины.py
@@ -2,42 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 with open("values_ADC_for_20mm_kalib.txt", "r") as data:
-    values_ADC_for_20mm_kalib = np.array([float(i) for i in data.read().split("\n")])
+    values_ADC_20 = np.array([float(i) for i in data.read().split("\n")])
 
 with open("values_ADC_for_40mm_kalib.txt", "r") as data:
-    values_ADC_for_40mm_kalib = np.array([float(i) for i in data.read().split("\n")])
+    values_ADC_40 = np.array([float(i) for i in data.read().split("\n")])
 
 with open("values_ADC_for_60mm_kalib.txt", "r") as data:
-    values_ADC_for_60mm_kalib = np.array([float(i) for i in data.read().split("\n")])
+    values_ADC_60 = np.array([float(i) for i in data.read().split("\n")])
 
 with open("values_ADC_for_80mm_kalib.txt", "r") as data:
-    values_ADC_for_80mm_kalib = np.array([float(i) for i in data.read().split("\n")])
+    values_ADC_80 = np.array([float(i) for i in data.read().split("\n")])
 
 with open("values_ADC_for_100mm_kalib.txt", "r") as data:
-    values_ADC_for_100mm_kalib = np.array([float(i) for i in data.read().split("\n")])
+    values_ADC_100 = np.array([float(i) for i in data.read().split("\n")])
 
 with open("values_ADC_for_120mm_kalib.txt", "r") as data:
-    values_ADC_for_120mm_kalib = np.array([float(i) for i in data.read().split("\n")])
+    values_ADC_120 = np.array([float(i) for i in data.read().split("\n")])
 
-average_values_for_ADC = np.array([sum(values_ADC_for_20mm_kalib)/len(values_ADC_for_20mm_kalib),
-                                    sum(values_ADC_for_40mm_kalib)/len(values_ADC_for_40mm_kalib),
-                                    sum(values_ADC_for_60mm_kalib)/len(values_ADC_for_60mm_kalib),
-                                    sum(values_ADC_for_80mm_kalib)/len(values_ADC_for_80mm_kalib),
-                                    sum(values_ADC_for_100mm_kalib)/len(values_ADC_for_100mm_kalib),
-                                    sum(values_ADC_for_120mm_kalib)/len(values_ADC_for_120mm_kalib)])
+srednee_values_ADC = np.array([sum(values_ADC_20)/len(values_ADC_20),
+                                    sum(values_ADC_40)/len(values_ADC_40),
+                                    sum(values_ADC_60)/len(values_ADC_60),
+                                    sum(values_ADC_80)/len(values_ADC_80),
+                                    sum(values_ADC_100)/len(values_ADC_100),
+                                    sum(values_ADC_120)/len(values_ADC_120)])
 
 deep = np.array([20, 40, 60, 80, 100, 120])
 
-sr_znach_ADC = sum(values_ADC_for_20mm_kalib**2)/5
+sr_znach_ADC = sum(values_ADC_20**2)/5
 sr_znach_deep = sum(deep**2)/5
 
-coefs = np.polyfit(average_values_for_ADC, deep, 1)#создаем коэффициенты
-func = np.poly1d(coefs)#создает функцию по этим коэффициентам
+coefs = np.polyfit(srednee_values_ADC, deep, 1)# коэффициенты
+func = np.poly1d(coefs)#функция по этим коэффициентам
 print("Коэффициенты:", func)
 
 
-plt.scatter(average_values_for_ADC, deep, c = '#0d00ff')
-plt.plot(average_values_for_ADC, func(average_values_for_ADC), label = "h = 99.13U -72.51", color = "red") #Написать в нахвание коэффициенты
+plt.scatter(srednee_values_ADC, deep, c = '#0d00ff')
+plt.plot(srednee_values_ADC, func(srednee_values_ADC), label = "h = 99.13U -72.51", color = "red") #Написать в названии коэффициенты
 plt.minorticks_on()
 
 plt.grid(which='major')
